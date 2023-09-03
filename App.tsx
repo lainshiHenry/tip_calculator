5e0024de683d464b62196eda1420e6b1b256924d
Henry Le <henryle913@outlook.com>
2023-09-03 02:46:29 -0500

    Implementing functionality

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 import SliderComp from './components/BaseComponents/SliderComp';
@@ -8,7 +8,6 @@ export default function App() {
   
   const [total, setTotal] = useState(0);
   const [subTotal, setSubTotal] = useState(0);
-  const [taxValue, setTaxValue] = useState(0);
   const [tipsValue, setTipsValue] = useState(0);
   const [tipsPerc, setTipsPerc] = useState(15);
 
@@ -16,56 +15,79 @@ export default function App() {
     if( subTotal <= 0 ){
       setTotal(0);
     } else {
-      let temp = subTotal + taxValue + tipsValue;
+      let temp = subTotal + tipsValue;
       console.log(temp);
       setTotal(temp);
     }
   }
 
+  function _updateTipsValue(){
+    if( subTotal <= 0){
+      setTipsValue(0);
+    } else {
+      setTipsValue(subTotal * (tipsPerc / 100));
+    }
+  }
+
   useEffect(() => {
+    _updateTipsValue();
     _updateValues();
-  }, [subTotal, taxValue, tipsValue])
+  }, [subTotal, tipsValue])
+
+  useEffect(() => {
+    _updateTipsValue();
+  }, [tipsPerc])
 
   return (
     <View style={styles.container}>
       {/* Text input for subtotal */}
-      <Text>SubTotal</Text>
-      <TextInputComp 
-        valueNum={subTotal}
-        isNumField={true}
-        onUpdateCallbackFn={(newValue: number) => {
-          // console.log(newValue);
-          setSubTotal(newValue);
-        }}
-      />
+      <View style={styles.topSection}>
+        <Text>SubTotal</Text>
+        <TextInputComp 
+          value={subTotal}
+          isNumField={true}
+          prependString='$'
+          onUpdateCallbackFn={(newValue) => {
+            console.log(`newValue: ${newValue}, typeof: ${typeof newValue}`);
+            // subTotal.current = newValue;
+            // console.log(newValue);
+            setSubTotal(newValue);
+          }}
+        />
 
-      {/* Text input for tips */}
-      <Text>Tips</Text>
-      <TextInputComp 
-        isEditable={false}
-        valueNum={tipsValue}
-      />
+        {/* Text input for tips */}
+        <Text>Tips: </Text>
+        <TextInputComp 
+          isEditable={false}
+          value={tipsValue}
+          prependString='$'
+        />
 
-      {/* Text input for total */}
-      <Text>Total</Text>
-      <TextInputComp 
-        isEditable={false}
-        valueNum={total}
-      />
+        {/* Text input for total */}
+        <Text>Total</Text>
+        <TextInputComp 
+          isEditable={false}
+          value={total}
+          prependString='$'
+        />
+      </View>
 
-      {/* Text input for tips */}
-      <TextInputComp
-        valueNum={tipsValue}
-        onUpdateCallbackFn={setTipsValue}
-      />
-
-      {/* Slider bar for tips */}
-      <SliderComp 
-        minValue={0}
-        maxValue={50}
-        currentValue={15}
-        onSliderCompleteCallback={setTipsValue}
-      />
+      <View style={styles.bottomSection}>
+        {/* Text input for tips */}
+        <Text>Tips %: </Text>
+        {/* Slider bar for tips */}
+        <SliderComp 
+          minValue={0}
+          maxValue={50}
+          currentValue={15}
+          onSliderCompleteCallback={setTipsPerc}
+        />  
+        <TextInputComp 
+          value={tipsPerc}
+          isEditable={false}
+          appendString='%'
+        />
+      </View>
 
       <StatusBar style="auto" />
     </View>
@@ -77,10 +99,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fefefe',
     alignItems: 'center',
     justifyContent: 'center',
   },
   topSection: {},
-  bottomSection: {}
+  bottomSection: {
+    width: '100%',
+    alignItems: 'center'
+  }
 });
--- components/BaseComponents/TextInputComp.tsx
+++ components/BaseComponents/TextInputComp.tsx
@@ -3,29 +3,46 @@ import { StyleSheet, TextInput } from 'react-native'
 
 export interface TextInputCompInterface{
   placeholder?: string,
-  valueNum?: number,
+  value?: number,
   onUpdateCallbackFn?: Function,
   isEditable?: boolean,
   isNumField?: boolean,
+  appendString?: string,
+  prependString?: string,
 }
 
 const TextInputComp = ({
   placeholder = '',
-  valueNum = 0,
+  value = 0.00,
   onUpdateCallbackFn = () => {},
   isEditable = true, 
   isNumField = false,
+  appendString = '',
+  prependString = '',
 }: TextInputCompInterface) => {
 
   return (
     <TextInput 
+      placeholder='$0.00'
       editable={isEditable}
-      value={valueNum.toString()}
-      onChangeText={(newValue) => {
-        if( newValue === ''){
-          onUpdateCallbackFn(0);
+      value={value === 0 || value === undefined ? '' : `${value.toFixed(2)}${appendString}`}
+      onKeyPress={(e) => {
+        console.log(e.nativeEvent.key);
+        console.log(e.nativeEvent.key === 'Backspace');
+
+        if( e.nativeEvent.key === 'Backspace' ){
+          console.log(`length of string: ${value.toString().length}`);
+          // if(value.toString().length - 1 === 0){
+          if(value === 0 || value.toString().length === 1 ){
+            onUpdateCallbackFn(0);
+          } else {
+            let temp = value.toString();
+            temp = temp.slice(0, temp.length - 1);
+            onUpdateCallbackFn(parseInt(temp));
+          }
         } else {
-          onUpdateCallbackFn(parseInt(newValue))
+            let temp = value.toString() + e.nativeEvent.key;
+            onUpdateCallbackFn(parseInt(temp));
         }
         
       }}
@@ -39,8 +56,10 @@ const styles = StyleSheet.create({
   textInput: {
     height: 40, 
     width: 150, 
+    borderWidth: 0, 
+    borderBottomWidth: 1,
+    borderBottomColor: 'black',
     borderStyle: 'solid', 
-    borderWidth: 1, 
     textAlign: 'center'
   }
 });
